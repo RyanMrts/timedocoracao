a217acd4f3f663a0fa3830dcd29362192591c52f
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-03-11 12:22:35 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Time do coração");
+Thread .Sleep(1000);
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("Corinthians");
+Console.ResetColor();
